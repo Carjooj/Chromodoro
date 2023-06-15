b7de63fb993a5dc427565b67c859972ec14919b8
Carjooj <victor@4info.com.br>
2023-06-15 17:33:04 -0300

    Criação das rotas de add e delete Exibindo os dados do banco front end da aba tarefas ok

--- app.js
+++ app.js
@@ -2,25 +2,59 @@ const express = require("express")
 const app = express()
 const handlebars = require("express-handlebars").engine
 const bodyParser = require("body-parser")
+const post = require("./models/tabelas")
 
-app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.engine("handlebars", handlebars({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(express.static('public'));
 
 
-app.get("/", function(req, res){
-    res.render("chromodoro")
+app.get("/", function (req, res) {
+    post.tarefas.findAll().then(function (post) {
+        res.render("chromodoro", { post })
+    })
+})
+
+app.post("/addtarefa", function (req, res) {
+    post.tarefas.create({
+        nome: req.body.nometarefa,
+        est_ciclo: req.body.estpomodoro,
+        descricao: req.body.descricao,
+        progresso: "Em andamento"
+    }).then(function () {
+        console.log("Tarefa Adicionada")
+        res.redirect("/")
+    }).catch(function (erro) {
+        res.send("Falha ao cadastrar os dados" + erro)
+    })
+})
+
+app.get("/excluirtarefa/:id_tarefa", function (req, res){
+    const id = req.params.id_tarefa
+    console.log(id)
+    post.tarefas.destroy({
+        where : {
+            id_tarefa : id
+        }
+    }).then(function(){
+        res.redirect("/")
+    }).catch(function(erro){
+        res.send("Erro ao excluir dados" + erro)
+    })
+})
+
+app.get("/conta", function (req, res){
+    res.render("conta")
 })
 
 
 
 
 
-
-app.listen(8081, function(){
+app.listen(8081, function () {
     console.log("Rodando")
 })
